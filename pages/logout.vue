6074ec65cbd477f29522b71d206f4918d92283a3
Damien Robinson <damien.robinson@daim.dev>
2022-06-27 22:30:14 +1000

    feat(pages): add logout

---
+++ pages/logout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="block p-6 max-w-sm m-auto">
+    <h1 class="text-3xl text-center">
+      <template v-if="loading">Logging</template>
+      <template v-if="!loading || !user">Logged</template>
+      out
+      <template v-if="user">{{ user.email }}</template>
+    </h1>
+  </div>
+</template>
+
+<script setup>
+// const supabase = useSupabaseClient();
+// const loading = ref(true);
+// const user = useSupabaseUser();
+// try {
+//   loading.value = true;
+//   let { error } = await supabase.auth.signOut();
+//   if (error) throw error;
+// } catch (error) {
+//   alert(error.message);
+// } finally {
+//   loading.value = false;
+// }
+</script>
