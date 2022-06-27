21faf47b9c61252c86a593708cde5f6ede1fa2a7
Damien Robinson <damien.robinson@daim.dev>
2022-06-27 18:00:27 +1000

    fix(styles): remove form control rounded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
         btn: 'py-2 px-4 px-6 py-2.5',
         'btn-link':
           'bg-transparent',
-        'form-control': 'block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
+        'form-control': 'block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
         'form-group': 'mb-6',
         'form-label': 'form-label inline-block mb-2 text-gray-700',
       },
