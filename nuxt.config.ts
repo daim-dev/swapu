249608821c6f2025d34745bfc4ac7ada3780f6ec
Damien Robinson <damien.robinson@daim.dev>
2022-06-27 22:06:39 +1000

    style: lint fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
-import invertColor from './utils/invert-color'
+import { defineNuxtConfig } from "nuxt";
+import invertColor from "./utils/invert-color";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: [
-    '@unocss/nuxt',
-  ],
-  css: ['@unocss/reset/tailwind.css'],
+  buildModules: ["@unocss/nuxt"],
+  css: ["@unocss/reset/tailwind.css"],
   unocss: {
     uno: true,
     attributify: true,
@@ -14,10 +12,10 @@ export default defineNuxtConfig({
     webFonts: {
       fonts: {
         brand: [
-          { name: 'Montserrat:400,700' },
+          { name: "Montserrat:400,700" },
           {
-            name: 'sans-serif',
-            provider: 'none',
+            name: "sans-serif",
+            provider: "none",
           },
         ],
       },
@@ -27,39 +25,41 @@ export default defineNuxtConfig({
     },
     theme: {
       colors: {
-        primary: '#ff3230',
-        nav: '#ffffff',
+        primary: "#ff3230",
+        nav: "#ffffff",
       },
     },
     shortcuts: [
       {
-        btn: 'py-2 px-4 px-6 py-2.5',
-        'btn-link':
-          'bg-transparent',
-        'form-control': 'block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
-        'form-group': 'mb-6',
-        'form-label': 'form-label inline-block mb-2 text-gray-700',
+        btn: "py-2 px-4 px-6 py-2.5",
+        "btn-link": "bg-transparent",
+        "form-control":
+          "block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
+        "form-group": "mb-6",
+        "form-label": "form-label inline-block mb-2 text-gray-700",
       },
       // dynamic shortcuts
       [
         /^btn-(.*)$/,
         ([, c], { theme }) => {
-          const color = theme.colors[c]
-          const sm = 'px-4 py-1.5'
-          const md = 'px-6 py-2.5'
-          const lg = 'px-8 py-3 text-md'
-          const sizes = { sm, md, lg }
-          const size = sizes[c]
+          const color = theme.colors[c];
+          const sm = "px-4 py-1.5";
+          const md = "px-6 py-2.5";
+          const lg = "px-8 py-3 text-md";
+          const sizes = { sm, md, lg };
+          const size = sizes[c];
           if (size) {
-            return size
+            return size;
           }
           const bg = color
             ? `bg-${c}-400 hover:bg-${c}-700 focus:bg-${c}-700 active:bg-${c}-800`
-            : ''
-          const text = color ? `text-${invertColor(color.DEFAULT ?? color, true)}` : ''
-          return `${bg} ${text} inline-block font-medium leading-tight shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out`
+            : "";
+          const text = color
+            ? `text-${invertColor(color.DEFAULT ?? color, true)}`
+            : "";
+          return `${bg} ${text} inline-block font-medium leading-tight shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out`;
         },
       ],
     ],
   },
-})
+});
