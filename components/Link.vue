2b03e47f04782956f201d713db7d91f483495d17
Damien Robinson <damien.robinson@daim.dev>
2022-06-27 17:11:46 +1000

    feat(components): add link

---
+++ components/Link.vue
@@ -0,0 +1,29 @@
+<template>
+  <a
+      v-if="url.startsWith('http')"
+      :href="url"
+      itemscope
+      itemtype="https://schema.org/SiteNavigationElement"
+      target="_blank"
+      rel="noopener"
+    >
+      <slot>{{ name }}</slot>
+    </a>
+    <NuxtLink
+      v-else
+      :to="url"
+      itemscope
+      itemtype="https://schema.org/SiteNavigationElement"
+    >
+      <slot>{{ name }}</slot>
+    </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    url: { type: String, default: null },
+    name: { type: String, default: null },
+  },
+};
+</script>
