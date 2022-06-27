3685e20fc5c3694b23f0c16e5ab53fde30863b60
Damien Robinson <damien.robinson@daim.dev>
2022-06-27 17:12:40 +1000

    feat(home page): add header section

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container mx-auto px-6 py-12 h-full w-full">
+    <Title v-if="title" itemprop="name">{{ title }}</Title>
+    <Meta
+      v-if="description"
+      name="description"
+      :content="description"
+      itemprop="description"
+    />
+    <div class="flex flex-auto flex-wrap">
+      <div class="w-full md:w-1/2 flex flex-col justify-center">
+        <h1 class="text-5xl font-brand font-bold">I'll Swap You</h1>
+        <p class="text-xl">Swap what you have for what you want</p>
+        <p>Swapping is the new online shopping</p>
+      </div>
+      <div class="w-full md:w-1/2">
+        <img
+          src="https://swapu.com.au/LandingPageOne/HeaderSectionGuest/Graphic.svg"
+          class="mx-auto"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "SwapU - The Swapping Marketplace",
+      description:
+        "Visit Australia's FREE online swap meet! Find cars, boats, motorbikes, furniture, electronics & more for swap across Australia.",
+    };
+  },
+};
+</script>
