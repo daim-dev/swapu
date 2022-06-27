18d288ffe99ba224d68ab20b475bbde3d3737c85
Damien Robinson <damien.robinson@daim.dev>
2022-06-27 23:33:05 +1000

    feat(products): fetch data from firestore

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -7,11 +7,13 @@
       :content="description"
       itemprop="description"
     />
-    <Meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/geosynergy-swapu.appspot.com/o/Banner%2FLOGO%20RED%20on%20transparent%20background.png?alt=media&token=81518040-5ec5-4265-bad1-73b0ebab100d"
+    <Meta
+      property="og:image"
+      content="https://firebasestorage.googleapis.com/v0/b/geosynergy-swapu.appspot.com/o/Banner%2FLOGO%20RED%20on%20transparent%20background.png?alt=media&token=81518040-5ec5-4265-bad1-73b0ebab100d"
     ></Meta>
     <div class="flex flex-auto flex-wrap max-w-1024px mx-auto">
       <div class="w-full md:w-1/2 flex flex-col justify-center">
-        <h1 class="text-5xl font-brand font-bold py-3">{{heading}}</h1>
+        <h1 class="text-5xl font-brand font-bold py-3">{{ heading }}</h1>
         <p class="text-xl py-3">Swap what you have for what you want</p>
         <p>Swapping is the new online shopping</p>
       </div>
@@ -23,11 +25,73 @@
         />
       </div>
     </div>
+    <div>
+      <pre v-if="error">Error{{ error }}</pre>
+      <Grid :items="items"></Grid>
+      <!-- <pre>{{ JSON.stringify(items, null, 4) }}</pre> -->
+    </div>
   </div>
 </template>
 
 <script>
+import normalizeData from "~/utils/normalize-data";
+function transformFirestore(item) {
+  const child = item?.fields ?? item?.values;
+  if (!child) {
+    return undefined;
+  }
+  if (item?.values) {
+    return item.values.map((item) => {
+      const value = item.mapValue ?? { fields: item }
+      return transformFirestore(value);
+    });
+  }
+  return Object.fromEntries(
+    Object.entries(child).map(([key, value]) => {
+      if (!value) {
+        return undefined;
+      }
+      const firstValue = Object.values(value)[0];
+      if (
+        typeof firstValue === "object" &&
+        !Array.isArray(firstValue) &&
+        firstValue !== null
+      ) {
+        return [key, transformFirestore(firstValue)];
+      }
+      if (Array.isArray(firstValue)) {
+        return [key, firstValue.map(transformFirestore)];
+      }
+      return [key, firstValue];
+    }),
+  );
+}
+function renameKeys(content) {
+  const renamed = normalizeData({
+    obj: content,
+    keysMap: {
+      // postDesc: "description",
+      postTitle: "name",
+      postId: "id",
+      postImages: "image",
+    },
+  });
+  renamed.image = renamed.image?.[0]?.url
+  return renamed
+}
 export default {
+  async setup() {
+    const url =
+      "https://firestore.googleapis.com/v1/projects/swapu-staging/databases/(default)/documents/All Posts";
+    const { data: items, error } = await useFetch(url, {
+      transform(data) {
+        return data.documents.map((item) => {
+          return renameKeys(transformFirestore(item));
+        });
+      },
+    });
+    return { items, error };
+  },
   data() {
     return {
       title: "SwapU - The Swapping Marketplace",
