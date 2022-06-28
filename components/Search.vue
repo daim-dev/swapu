ae5c54e7e47b273c586e4cbae6f4c6e1616178b6
Damien Robinson <damien.robinson@daim.dev>
2022-06-28 13:12:56 +1000

    fix(search): add boder black

--- components/Search.vue
+++ components/Search.vue
@@ -6,13 +6,13 @@
       >
         <input
           type="search"
-          class="form-control w-1% relative flex-auto min-w-0 flex px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          class="form-control w-1% relative flex-auto min-w-0 flex px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-black-600 focus:outline-none"
           placeholder="Search"
           aria-label="Search"
           aria-describedby="button-addon2"
         />
         <button
-          class="btn relative border border-gray-300 inline-block -ml-1px px-6 py-2.5 text-black hover:text-white font-medium text-xs leading-tight uppercase hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+          class="btn border border-gray-300 inline-block -ml-1px px-6 py-2.5 text-black hover:text-white active:text-white focus:text-white font-medium text-xs leading-tight uppercase hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
           type="button"
         >
           <span class="i-mdi-search h-24px w-24px my-auto"></span>
