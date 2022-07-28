70c95256f8bd518569d35fa6c35cb72fe24a63a8
Damien Robinson <damien.robinson@daim.dev>
2022-07-28 20:26:06 +1000

    feat(footer): add logo, social and email input

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,37 @@
 <template>
-  <section class="bg-nav min-h-64px flex justify-center items-center px-4">
-    <p>
-      Swapu Copyright © 2022 SwapU All Rights Reserved. | Site By
+  <section
+    class="bg-footer flex justify-center items-center text-white flex-wrap px-8 md:px-32 py-14"
+  >
+    <div class="flex flex-wrap w-full justify-between">
+      <div class="flex justify-between">
+        <img src="/img/footer-logo.svg" alt="Swapu" width="122" height="35" />
+        <div class="flex items-center ml-15">
+          <img src="/img/social/instagram.png" alt="Instagram" class="w-25px h-25px mr-3" height="25" width="25" />
+          <img src="/img/social/facebook.png" alt="Facebook" class="w-25px h-25px mr-3" height="25" width="25" />
+          <img src="/img/social/twitter.png" alt="Twitter" class="w-25px h-25px" height="25" width="25" />
+        </div>
+      </div>
+      <div class="font-semibold">Contact</div>
+      <div class="flex w-full md:w-auto flex-wrap">
+        <div class="w-full sm:w-1/2">
+          <div class="font-semibold">Don't miss out</div>
+          <div class="opacity-50">Receive weekly swap updates</div>
+        </div>
+        <div class="flex w-full sm:w-1/2">
+          <input
+            class="form-control border-none rounded-l-lg"
+            type="email"
+            placeholder="Your email address"
+          />
+          <button class="btn btn-primary rounded-r">Subscribe</button>
+        </div>
+      </div>
+    </div>
+    <hr class="w-full opacity-30 mt-8" />
+    <p class="opacity-50">
+      Copyright © 2022 SwapU | <a class="underline">Privacy Policy</a> |
+      <a class="underline">Terms and Conditions</a> |
+      <a class="underline">Cookie Policy</a> | Site By
       <a href="https://daim.dev" target="_blank" class="underline"
         >Daim Digital</a
       >
