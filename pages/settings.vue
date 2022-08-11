7ff6cad13ec73c457c85206f492d14c6f436694d
Damien Robinson <damien.robinson@daim.dev>
2022-08-11 14:23:37 +1000

    fix(language): add english toggle

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="container mx-auto px-6 py-6 h-full w-full">
-    <div>
-      <div class="uppercase text-[#92989E] p-2 font-semibold">Settings</div>
-      <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
-        <NuxtLink to="/settings/language">Language</NuxtLink>
+    <div class="flex">
+      <div>
+        <div class="uppercase text-[#92989E] p-2 font-semibold text-sm">Settings</div>
+        <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
+          <NuxtLink to="/settings/language">Language</NuxtLink>
+        </div>
+        <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
+          <NuxtLink to="/settings/push-notifications"
+            >Push notifications</NuxtLink
+          >
+        </div>
+        <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
+          <NuxtLink to="/settings/change-password">Change password</NuxtLink>
+        </div>
+        <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
+          <NuxtLink to="/settings/faq">FAQ</NuxtLink>
+        </div>
+        <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
+          <NuxtLink to="/settings/support">Support</NuxtLink>
+        </div>
+        <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
+          <NuxtLink to="/settings/payment">Payment</NuxtLink>
+        </div>
       </div>
-      <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
-        <NuxtLink to="/settings/push-notifications">Push notifications</NuxtLink>
+      <div class="flex flex-auto">
+        <NuxtPage></NuxtPage>
       </div>
-      <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
-        <NuxtLink to="/settings/change-password">Change password</NuxtLink>
-      </div>
-      <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
-        <NuxtLink to="/settings/faq">FAQ</NuxtLink>
-      </div>
-      <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
-        <NuxtLink to="/settings/support">Support</NuxtLink>
-      </div>
-      <div class="font-semibold my-2 text-lg py-2 px-4 text-[#6F767E]">
-        <NuxtLink to="/settings/payment">Payment</NuxtLink>
-      </div>
-    </div>
-    <div>
-      <NuxtPage></NuxtPage>
     </div>
   </div>
 </template>
@@ -32,16 +36,16 @@ export default {
   data: () => ({
     sidebar: [
       {
-        url: '/settings/language',
-        name: 'Language',
-        pos: '1/1'
+        url: "/settings/language",
+        name: "Language",
+        pos: "1/1",
       },
       {
-        url: '/settings/push-notifications',
-        name: 'Push notifications',
-        pos: '2/1'
-      }
-    ]
+        url: "/settings/push-notifications",
+        name: "Push notifications",
+        pos: "2/1",
+      },
+    ],
   }),
 };
 </script>
--- pages/settings/language.vue
+++ pages/settings/language.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
-    <h1 class="font-semibold text-2xl">
-      Language
-    </h1>
+  <div class="w-full">
+    <h1 class="font-semibold text-2xl">Language</h1>
+    <div class="w-full mt-8">
+      <div class="flex flex-row">
+        <span class="font-semibold mr-auto">English</span>
+        <label
+          for="english-toggle"
+          class="inline-flex relative items-center cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            value=""
+            id="english-toggle"
+            class="sr-only peer"
+          />
+          <div
+            class="peer-checked w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[' '] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"
+          ></div>
+          <span
+            class="ml-3 font-semibold text-gray-900 dark:text-gray-300 sr-only"
+            >English</span
+          >
+        </label>
+      </div>
+    </div>
+    <button class="btn btn-primary rounded-lg font-semibold mt-8">Save</button>
   </div>
 </template>
+
+<style>
+.peer-checked:after {
+  content: "";
+}
+</style>
