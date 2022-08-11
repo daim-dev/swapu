e2bb25058b8538ef9b2a6c9947d91f10edb9f36e
Damien Robinson <damien.robinson@daim.dev>
2022-08-11 15:28:21 +1000

    feat(settings): add push notifications toggle

--- pages/settings/push-notifications.vue
+++ pages/settings/push-notifications.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>
-    <h1 class="font-semibold text-2xl">
-      Push Notifications
-    </h1>
+  <div class="w-full">
+    <h1 class="font-semibold text-2xl">Push Notifications</h1>
+    <div class="w-full mt-8">
+      <div class="flex flex-row">
+        <span class="font-semibold mr-auto">Push Notification</span>
+        <label
+          for="push-notification-toggle"
+          class="inline-flex relative items-center cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            value=""
+            id="push-notification-toggle"
+            class="sr-only peer"
+          />
+          <div
+            class="peer-checked w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[' '] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"
+          ></div>
+          <span
+            class="ml-3 font-semibold text-gray-900 dark:text-gray-300 sr-only"
+            >Push Notification</span
+          >
+        </label>
+      </div>
+      <div class="flex flex-row pt-8">
+        <span class="font-semibold mr-auto">Email Notification</span>
+        <label
+          for="email-notification-toggle"
+          class="inline-flex relative items-center cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            value=""
+            id="email-notification-toggle"
+            class="sr-only peer"
+          />
+          <div
+            class="peer-checked w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[' '] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"
+          ></div>
+          <span
+            class="ml-3 font-semibold text-gray-900 dark:text-gray-300 sr-only"
+            >Email Notification</span
+          >
+        </label>
+      </div>
+    </div>
+    <button class="btn btn-primary rounded-lg font-semibold mt-8">Save</button>
   </div>
 </template>
