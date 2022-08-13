630e0b80258634f522cfc9824f2de65639bcf415
Damien Robinson <damien.robinson@daim.dev>
2022-08-13 21:40:22 +1000

    fix(language): english checked default

--- pages/settings/language.vue
+++ pages/settings/language.vue
@@ -13,6 +13,7 @@
             value=""
             id="english-toggle"
             class="sr-only peer"
+            checked
           />
           <div
             class="peer-checked w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[' '] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"
