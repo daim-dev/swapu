661a1484e1e6f3d8648b7e23703aa2ce81ee757d
Damien Robinson <damien.robinson@daim.dev>
2022-08-25 15:50:29 +1000

    fix(profile): add user and profile data

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container mx-auto px-6 py-12 h-full w-full">
-    <form
-      class="w-full md:w-1/2 p-6 m-auto"
-      @submit.prevent="updateProfile"
-    >
+    <form class="w-full md:w-1/2 p-6 m-auto" @submit.prevent="updateProfile">
+      <client-only>
         <div class="form-group">
           <label for="email">Email</label>
           <input
@@ -15,6 +13,13 @@
           />
         </div>
         <div v-if="profile" class="form-group">
+        <label for="givenName">User Name</label>
+          <input
+            id="userName"
+            type="text"
+            v-model="profile.userName"
+            class="form-control mb-3"
+          />
           <label for="givenName">First Name</label>
           <input
             id="givenName"
@@ -31,90 +36,34 @@
           />
         </div>
 
-      <div class="form-group">
-        <button
-          type="submit"
-          class="btn btn-primary block w-full rounded-lg"
-          :disabled="loading"
-        >
-          {{ loading ? "Loading ..." : "Save" }}
-        </button>
-      </div>
+        <div class="form-group">
+          <button
+            type="submit"
+            class="btn btn-primary block w-full rounded-lg"
+            :disabled="loading"
+          >
+            {{ loading ? "Loading ..." : "Save" }}
+          </button>
+        </div>
+      </client-only>
     </form>
   </div>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from "vue";
+import { useUserStore } from "~/stores/user";
 
 export default defineComponent({
   async setup() {
     const loading = ref(true);
-    const user = ref(true);
-
-    async function getProfile() {
-      const defaultProfile = {
-        givenName: "",
-        familyName: "",
-      };
-      try {
-        loading.value = true;
-
-        if (user.value && user.value.id) {
-          const { data, error, status } = await supabase
-            .from("persons")
-            .select("givenName, familyName")
-            .eq("id", user.value.id)
-            .single();
-
-          if (error && status !== 406) throw error;
-
-          if (data) {
-            return data;
-          }
-        }
-        return defaultProfile;
-      } catch (error) {
-        console.error(error.message);
-        return defaultProfile;
-      } finally {
-        loading.value = false;
-      }
-    }
-
-    async function updateProfile() {
-      try {
-        loading.value = true;
-
-        const updates = {
-          id: user.value.id,
-          givenName: profile.value.givenName,
-          familyName: profile.value.familyName,
-          updatedAt: new Date(),
-        };
-
-        let { error } = await supabase.from("persons").upsert(updates, {
-          returning: "minimal", // Don't return the value after inserting
-        });
-
-        if (error) throw error;
-      } catch (error) {
-        alert(error.message);
-      } finally {
-        loading.value = false;
-      }
-    }
-
-    const { data: profile } = await useAsyncData(
-      "user-profile",
-      // async () => await getProfile(),
-      () => ({
-        givenName: "",
-        familyName: "",
-      }),
-    );
+    const store = useUserStore();
+    const profile = store.profile;
+    const user = store.user;
 
     loading.value = false;
+
+    function updateProfile() {}
 
     return {
       profile,
