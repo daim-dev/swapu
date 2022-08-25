cbaa2981c8716722f798d59e5ff99b1556947a7d
Damien Robinson <damien.robinson@daim.dev>
2022-08-25 14:31:54 +1000

    feat: add account created page

---
+++ pages/account-created.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="block p-6 m-auto">
+    <h1 class="text-5xl text-center font-semibold leading-[64px]">
+      <div class="text-primary">
+        Congratulations!
+      </div>
+      <div>
+        You have created an account
+      </div>
+    </h1>
+    <nuxt-link class="btn btn-primary rounded-lg text-center mt-16 w-full" to="/">Proceed</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
