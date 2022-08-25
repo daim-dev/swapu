84e6326a627f6a7d86311e7b425e0fb2cc905ded
Damien Robinson <damien.robinson@daim.dev>
2022-08-25 15:56:14 +1000

    feat(profile items): add item button

--- pages/profile/items.vue
+++ pages/profile/items.vue
@@ -1,5 +1,8 @@
 <template>
-<div>
-  <h1 class="font-semibold text-2xl">My items</h1>
-</div>
+  <div class="container mx-auto px-6 py-12 h-full w-full">
+    <div class="flex justify-between">
+      <h1 class="font-semibold text-2xl">My items</h1>
+      <nuxt-link class="btn btn-primary rounded-lg" to="/create">Add item</nuxt-link>
+    </div>
+  </div>
 </template>
