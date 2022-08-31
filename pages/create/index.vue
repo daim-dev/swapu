f8c37c03291ed1e6c7661012395122b250fac983
Damien Robinson <damien.robinson@daim.dev>
2022-08-31 15:29:32 +1000

    fix(create): more heading padding

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto px-6 py-12 h-full w-full">
     <div class="flex flex-auto flex-wrap max-w-1024px mx-auto">
       <div class="w-full flex flex-col justify-center">
-        <h1 class="text-4xl font-semibold py-3 text-center">
+        <h1 class="text-4xl font-semibold py-9 text-center">
           What are you planning to Swap?
         </h1>
       </div>
