c7c55e99a9609751df4e847f1b07d911d797f35e
Damien Robinson <damien.robinson@daim.dev>
2022-09-19 01:03:58 +1000

    fix(create swap ready): add subscription modal

--- pages/create/swap-ready.vue
+++ pages/create/swap-ready.vue
@@ -19,9 +19,7 @@
       </span>
       <span class="flex">back</span>
     </nuxt-link>
-    <div
-      class="flex flex-auto flex-wrap mx-auto bg-white p-8 rounded-2xl mt-6"
-    >
+    <div class="flex flex-auto flex-wrap mx-auto bg-white p-8 rounded-2xl mt-6">
       <div class="text-center w-full md:w-1/2">
         <h1 class="text-5xl font-semibold text-[#0D284F] mb-3">
           Tell everyone your listing is ready to swap
@@ -35,18 +33,18 @@
         </h2>
         <div class="mx-3">
           <ul class="list-disc text-[#0D284F] text-left text-lg">
-          <li class="mb-3">
-            An E-book that contains all the information that you need to swap
-            any registered vehicle, boat or trailer.
-          </li>
-          <li class="mb-3">
-            A “Swap Ready” badge to show other swappers that your item is ready
-            to swap.
-          </li>
-        </ul>
-        <p class="text-lg">
-          Make your swapped item stand out. Buy a “Swap Ready” package today!
-        </p>
+            <li class="mb-3">
+              An E-book that contains all the information that you need to swap
+              any registered vehicle, boat or trailer.
+            </li>
+            <li class="mb-3">
+              A “Swap Ready” badge to show other swappers that your item is
+              ready to swap.
+            </li>
+          </ul>
+          <p class="text-lg">
+            Make your swapped item stand out. Buy a “Swap Ready” package today!
+          </p>
         </div>
       </div>
       <div class="w-full md:w-1/2">
@@ -58,13 +56,60 @@
           to="/create/swap-for"
           >Skip</nuxt-link
         >
-        <nuxt-link
+        <button
           class="btn btn-primary rounded-2xl font-semibold text-center py-4"
-          to="/create/swap-for"
-          >Add a "Swap Ready" package</nuxt-link
+          @click="showModal = true"
         >
+          Add a "Swap Ready" package
+        </button>
       </div>
     </div>
+    <Modal v-model:open="showModal" title="Subscription payment">
+      <div class="mt-9">
+        <div class="w-full mb-6">
+          <label
+            for="credit-card-number"
+            class="form-label inline-block mb-2 text-gray-700 text-lg font-semibold mb-4"
+            >Credit card</label
+          >
+          <input
+            type="number"
+            class="form-control text-xl bg-[#F4F4F4] focus:bg-[#F4F4F4] font-semibold border-none rounded-lg py-3 px-4"
+            id="credit-card-number"
+            placeholder="Card number"
+          />
+        </div>
+        <div class="flex">
+          <div class="w-full md:w-1/2 mb-6 md:mr-4">
+            <label
+              for="expiration-date"
+              class="form-label inline-block mb-2 text-gray-700 text-lg font-semibold mb-4"
+              >Expiration date</label
+            >
+            <input
+              type="number"
+              class="form-control text-xl bg-[#F4F4F4] focus:bg-[#F4F4F4] font-semibold border-none rounded-lg py-3 px-4"
+              id="expiration-date"
+              placeholder="mm/yy"
+            />
+          </div>
+          <div class="w-full md:w-1/2 mb-6">
+            <label
+              for="cvv"
+              class="form-label inline-block mb-2 text-gray-700 text-lg font-semibold mb-4"
+              >Security code</label
+            >
+            <input
+              type="number"
+              class="form-control text-xl bg-[#F4F4F4] focus:bg-[#F4F4F4] font-semibold border-none rounded-lg py-3 px-4"
+              id="cvv"
+              placeholder="CVV"
+            />
+          </div>
+        </div>
+        <button class="btn btn-primary rounded-lg w-180px font-semibold text-lg">Add</button>
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -72,6 +117,7 @@
 export default {
   data() {
     return {
+      showModal: false,
       title: "SwapU - The Swapping Marketplace",
       heading: "Create a Listing",
       description:
