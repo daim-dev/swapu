5904eef955608e764b3c9e2ab8e27e3681b198da
Damien Robinson <damien.robinson@daim.dev>
2022-09-19 15:50:09 +1000

    fix(create index): add selected border

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -7,14 +7,16 @@
       :content="description"
       itemprop="description"
     />
-    <Meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/geosynergy-swapu.appspot.com/o/Banner%2FLOGO%20RED%20on%20transparent%20background.png?alt=media&token=81518040-5ec5-4265-bad1-73b0ebab100d"
+    <Meta
+      property="og:image"
+      content="https://firebasestorage.googleapis.com/v0/b/geosynergy-swapu.appspot.com/o/Banner%2FLOGO%20RED%20on%20transparent%20background.png?alt=media&token=81518040-5ec5-4265-bad1-73b0ebab100d"
     ></Meta>
     <nuxt-link class="flex" to="/">
       <span class="bg-[#E9ECF8] mr-2 my-auto flex rounded-full w-26px h-26px">
-          <span
-            class="text-primary w-16px h-16px m-auto i-carbon-chevron-left"
-          ></span>
-        </span>
+        <span
+          class="text-primary w-16px h-16px m-auto i-carbon-chevron-left"
+        ></span>
+      </span>
       <span class="flex">back</span>
     </nuxt-link>
     <div class="flex flex-auto flex-wrap mx-auto bg-white p-8 rounded-2xl mt-6">
@@ -24,14 +26,36 @@
         </h1>
       </div>
       <div class="w-full flex justify-center items-center">
-        <div class="text-center bg-[#F7F7F7] p-4 grow max-w-280px mx-7 rounded-lg">
+        <label
+          class="text-center bg-[#F7F7F7] p-4 grow max-w-280px mx-7 rounded-lg"
+          :class="{'border border-primary': multiple === 'single'}"
+        >
+          <input
+            type="radio"
+            name="multiple"
+            id="create-single"
+            class="peer hidden"
+            v-model="multiple"
+            value="single"
+          />
           <img src="/img/create/single.png" class="mx-auto" alt="" />
           <div class="font-semibold py-4">One item</div>
-        </div>
-        <div class="bg-[#F7F7F7] p-4 flex flex-col grow max-w-280px mx-7 rounded-lg">
+        </label>
+        <label
+          class="bg-[#F7F7F7] p-4 flex flex-col grow max-w-280px mx-7 rounded-lg"
+          :class="{'border border-primary': multiple === 'multiple'}"
+        >
+          <input
+            type="radio"
+            name="multiple"
+            id="create-multiple"
+            value="multiple"
+            class="hidden"
+            v-model="multiple"
+          />
           <img src="/img/create/multiple.png" class="mx-auto" alt="" />
           <div class="font-semibold py-4">Multiple items in a bundle swap</div>
-        </div>
+        </label>
       </div>
       <div class="w-full flex flex-col justify-center items-center mt-14">
         <nuxt-link
@@ -45,14 +69,15 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        title: "SwapU - The Swapping Marketplace",
-        heading: "Create a Listing",
-        description:
-          "Visit Australia's FREE online swap meet! Find cars, boats, motorbikes, furniture, electronics & more for swap across Australia.",
-      };
-    },
-  };
-  </script>
+export default {
+  data() {
+    return {
+      multiple: 'single',
+      title: "SwapU - The Swapping Marketplace",
+      heading: "Create a Listing",
+      description:
+        "Visit Australia's FREE online swap meet! Find cars, boats, motorbikes, furniture, electronics & more for swap across Australia.",
+    };
+  },
+};
+</script>
