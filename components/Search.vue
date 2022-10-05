3a4a3d96ba72471ee2ee6e7fd2e3ce33c90fe335
Damien Robinson <damien.robinson@daim.dev>
2022-10-05 17:04:22 +1000

    fix(search): update icon

--- components/Search.vue
+++ components/Search.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="justify-center">
-    <div class="xl:w-96 mx-auto">
-      <div
-        class="input-group relative flex flex-wrap items-stretch w-full"
-      >
-        <input
-          type="search"
-          class="form-control w-1% relative flex-auto min-w-0 flex px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-black-600 focus:outline-none"
-          placeholder="Search"
-          aria-label="Search"
-          aria-describedby="button-addon2"
-        />
+    <div class="mx-auto">
+      <div class="input-group relative flex flex-wrap items-stretch w-full border border-solid border-[#E9E9E9] rounded-2xl bg-[#F8F8F8] overflow-hidden">
         <button
-          class="btn border border-gray-300 inline-block -ml-1px px-6 py-2.5 text-black hover:text-white active:text-white focus:text-white font-medium text-xs leading-tight uppercase hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+          class="btn border-none inline-block -ml-1px pl-6 px-0 bg-[#F8F8F8] py-2.5 text-[#373637] focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex items-center"
           type="button"
         >
-          <span class="i-mdi-search h-24px w-24px my-auto"></span>
+          <span class="i-fe-search h-24px w-24px my-auto"></span>
         </button>
+        <input
+          type="search"
+          class="form-control w-1% relative flex-auto min-w-0 flex px-3 py-6 text-lg font-normal text-[#787878] bg-[#F8F8F8] bg-clip-padding transition ease-in-out m-0 focus:text-[#787878] focus:bg-white border-none focus:outline-none"
+          placeholder="I'm looking for..."
+          aria-label="I'm looking for..."
+        />
       </div>
     </div>
   </div>
