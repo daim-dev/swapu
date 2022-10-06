b58ff832b819f0407fb4c0fa8b15efbb65296718
Damien Robinson <damien.robinson@daim.dev>
2022-10-06 22:12:55 +1000

    fix(listing page): add date

--- pages/listings/[id].vue
+++ pages/listings/[id].vue
@@ -7,24 +7,61 @@
       :content="description"
       itemprop="description"
     />
-    <Meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/geosynergy-swapu.appspot.com/o/Banner%2FLOGO%20RED%20on%20transparent%20background.png?alt=media&token=81518040-5ec5-4265-bad1-73b0ebab100d"
+    <Meta
+      property="og:image"
+      content="https://firebasestorage.googleapis.com/v0/b/geosynergy-swapu.appspot.com/o/Banner%2FLOGO%20RED%20on%20transparent%20background.png?alt=media&token=81518040-5ec5-4265-bad1-73b0ebab100d"
     ></Meta>
     <div class="flex flex-auto flex-wrap max-w-1024px mx-auto" v-if="item">
-    <div class="w-full md:w-1/2 pr-4">
-        <img
-          :src="item.image"
-          class="mx-auto"
-          alt=""
-        />
+      <div class="w-full md:w-1/2 pr-4">
+        <img :src="item.image" class="mx-auto rounded-lg" alt="" />
       </div>
-      <div class="w-full md:w-1/2 flex flex-col justify-center pl-4">
-        <h1 class="text-5xl font-brand font-bold py-3">{{item.name}}</h1>
-        <p class="text-2xl py-3">${{item.price}}</p>
-        <p class="text-gray-600">
-          <span class="i-carbon-location my-auto inline-block"></span>
-          {{item.location}}
-        </p>
-        <p class="text-xl py-3">{{item.description}}</p>
+      <div class="w-full md:w-1/2 flex flex-col pl-4">
+        <div class="flex justify-between mb-5">
+          <h1 class="text-3xl font-brand font-bold py-3">{{ item.name }}</h1>
+          <p class="text-3xl py-3 text-primary font-bold">${{ item.price }}</p>
+        </div>
+        <div class="flex">
+          <p class="text-gray-600 mr-9 flex">
+            <span class="mr-2 i-quill-calendar-more my-auto"></span>
+            {{
+              new Date(item.created_on).toLocaleDateString(undefined, {
+                day: "numeric",
+                month: "short",
+                year: 'numeric'
+              })
+            }}
+          </p>
+          <p class="text-gray-600 flex">
+            <span class="i-carbon-location mr-2 my-auto"></span>
+            {{ item.location }}
+          </p>
+        </div>
+
+        <p class="py-3">{{ item.description }}</p>
+
+        <div>
+          <h3 class="font-bold">Will swap for</h3>
+          <button
+            class="bg-[#7B7C7D]/10 font-bold py-1 px-5 text-[#7B7C7D] rounded-full"
+          >
+            Cars
+          </button>
+        </div>
+
+        <h3 class="font-bold">Swap/Sell Preferences</h3>
+        <div class="flex justify-between">
+          <button class="btn btn-primary rounded font-semibold">
+            Make a Swap Offer
+          </button>
+          <button class="btn bg-[#0D284F] rounded text-light font-semibold">
+            Make a Cash Offer
+          </button>
+          <button
+            class="btn border border-color-[#606468] rounded font-semibold text-[#606468]"
+          >
+            Message
+          </button>
+        </div>
       </div>
     </div>
     <div>
@@ -34,19 +71,19 @@
 </template>
 
 <script>
-import transformFirestore from '~/utils/transform-firestore';
-import renameKeys from '~/utils/remap-listing-keys';
-export default {
+import transformFirestore from "~/utils/transform-firestore";
+import renameKeys from "~/utils/remap-listing-keys";
+export default defineComponent({
   async setup() {
-    const route = useRoute()
-    const url =
-      `https://firestore.googleapis.com/v1/projects/swapu-staging/databases/(default)/documents/All Posts/${route.params.id}`;
+    const route = useRoute();
+    const url = `https://firestore.googleapis.com/v1/projects/swapu-staging/databases/(default)/documents/All Posts/${route.params.id}`;
     const { data: item, error } = await useFetch(url, {
-      // server: false,
+      key: url,
+      server: false,
       transform(data) {
-        console.log(data)
+        console.log(data);
         const transformed = renameKeys(transformFirestore(data));
-        console.log('transformed', transformed)
+        console.log("transformed", transformed);
         return transformed;
       },
     });
@@ -60,5 +97,5 @@ export default {
         "Visit Australia's FREE online swap meet! Find cars, boats, motorbikes, furniture, electronics & more for swap across Australia.",
     };
   },
-};
+});
 </script>
